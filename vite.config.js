17de3db881d79b9692157285209ac58870ae127e
Conor Sinclair <conor@sinclair.software>
2024-02-14 18:30:50 +0000

    added vitest and basic test

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+
+/** @type {import('vite').UserConfig} */
+const config = {
+  plugins: [svelte()],
+  test: {
+    include: ["src/**/*.{test,spec}.{js,ts}"],
+  },
+};
+
+export default config;
