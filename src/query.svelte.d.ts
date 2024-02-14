5a4515bb172118fb9a6f559170b40c9d50db09ac
Conor Sinclair <conor@sinclair.software>
2024-02-14 18:18:19 +0000

    implemented the query fetching

--- src/query.svelte.d.ts
+++ src/query.svelte.d.ts
@@ -1,6 +1,6 @@
 type Fn = (...args: any[]) => Promise<void>;
 
-type Cache = {
+type CacheObject = {
   [key: string]: {
     /**
      * The time the data expires and needs to be refreshed
@@ -14,5 +14,16 @@ type Cache = {
      * A function that will fetch the data and update the cache record
      */
     fetcher: () => void;
+  };
+};
+
+type Queryable<T = any> = {
+  /**
+   *
+   * @param args -- any arguments that the fetcher function needs
+   * @returns An object with a _reactive_ data getter
+   */
+  query: (...args: any[]) => {
+    data: T;
   };
 };
--- src/query.svelte.js
+++ src/query.svelte.js
@@ -1,13 +1,72 @@
 /**
- * @type {Cache}
+ * @type {CacheObject}
  */
 let cache = $state({});
+
+const FIVE_MINUTES = 1000 * 60 * 5;
 
 /**
  *
  * @param {Fn} fn
- * @returns
+ * @param {number | undefined} expiry
+ * @returns {Queryable}
  */
-export function createQuery(fn) {
-  return;
+export function createQuery(fn, expiry = FIVE_MINUTES) {
+  // a unique salt for each createQuery invocation
+  const salt = Math.random().toString(36).substring(7);
+
+  return {
+    query(...args) {
+      const key = serialize(fn, args, salt);
+
+      if (cache[key]) {
+        // check if the cache key has expired and is indeed of revalidation
+        if (cache[key].expiry < Date.now()) {
+          cache[key].fetcher();
+        } else {
+          // if not expired, return the cached data
+          return {
+            get data() {
+              return cache[key].data;
+            },
+          };
+        }
+      }
+
+      // the fetcher uses closures to re-run the same query with the same args when needed
+      function fetcher() {
+        f(...args).then((result) => {
+          // this is reactive, so mutating the data will be picked up by Svelte
+          cache[key].data = result;
+        });
+      }
+      // immediately trigger the fetcher
+      fetcher();
+
+      // add to the cache
+      cache[key] = {
+        expiry: Date.now() + expiry,
+        data: undefined,
+        fetcher,
+      };
+
+      // return reactive data for consumers
+      return {
+        get data() {
+          return cache[key].data;
+        },
+      };
+    },
+  };
+}
+
+/**
+ *
+ * @param {Fn} f
+ * @param {any[]} a
+ * @param {string} salt
+ * @returns {string}
+ */
+function serialize(f, a, salt) {
+  return ((f && f["name"]) || "anonymous") + salt + JSON.stringify(a || "");
 }
