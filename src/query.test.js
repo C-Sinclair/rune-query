8b3f07fd14b33ac2778c78f1d9fc42b28aebdbed
Conor Sinclair <conor@sinclair.software>
2024-02-14 18:38:48 +0000

    start on test suite and build file

--- src/query.test.js
+++ src/query.test.js
@@ -1,7 +1,20 @@
-import { describe, it, expect } from "vitest";
+import { describe, test, expect, vi } from "vitest";
+import { createQuery } from "./query.svelte.js";
 
-describe("sum test", () => {
-  it("adds 1 + 2 to equal 3", () => {
-    expect(1 + 2).toBe(3);
+describe("createQuery", () => {
+  test("does reactivity work here?", () => {
+    let { query } = createQuery(async () => "hello");
+    let q = query();
+    expect(q.data).toBe("hello");
+  });
+
+  test("invalidate calls the fetcher", () => {
+    const spy = vi.fn(async () => "hello");
+    let { query, invalidate } = createQuery(spy);
+
+    let q = query();
+    invalidate();
+    expect(q.data).toBe("hello");
+    expect(spy).toHaveBeenCalledTimes(2);
   });
 });
--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,21 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('vite').UserConfig} */
 const config = {
-  plugins: [svelte()],
+  plugins: [
+    svelte({
+      compilerOptions: {
+        runes: true,
+      },
+    }),
+  ],
   test: {
     include: ["src/**/*.{test,spec}.{js,ts}"],
+  },
+  build: {
+    lib: {
+      entry: "src/index.js",
+      name: "rune-query",
+    },
   },
 };
 
