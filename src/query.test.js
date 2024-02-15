cda88f3418c86a57c6d3a690dd88cd37622e1251
Conor Sinclair <conor@sinclair.software>
2024-02-15 08:23:29 +0000

    fixed test suite

--- src/query.test.js
+++ src/query.test.js
@@ -2,19 +2,29 @@ import { describe, test, expect, vi } from "vitest";
 import { createQuery } from "./query.svelte.js";
 
 describe("createQuery", () => {
-  test("does reactivity work here?", () => {
+  test("does reactivity work here?", async () => {
     let { query } = createQuery(async () => "hello");
     let q = query();
+    expect(q.data).toBe(undefined);
+    await waitForData(q);
     expect(q.data).toBe("hello");
   });
 
-  test("invalidate calls the fetcher", () => {
+  test("invalidate calls the fetcher", async () => {
     const spy = vi.fn(async () => "hello");
     let { query, invalidate } = createQuery(spy);
 
     let q = query();
+    await waitForData(q);
+
     invalidate();
     expect(q.data).toBe("hello");
     expect(spy).toHaveBeenCalledTimes(2);
   });
 });
+
+async function waitForData(q) {
+  await vi.waitFor(() => {
+    if (!q.data) throw new Error("no data yet");
+  });
+}
